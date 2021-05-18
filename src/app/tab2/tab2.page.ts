ff7b7ce611a1fc994c32aa7a456d93fbd26bc7fd
Raúl Sulca Palacios <raul_97_02@hotmail.com>
2021-05-18 00:08:37 -0500

    Init ready

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  constructor() {
+    console.log('Hello from tab2 con.');
+  }
+  
+  ngOnInit(){
+    console.log('Hello from tab2 OnInit.');
+    let num = 10;
+    num=49;
+    num+=10;
+    num = 7;
+    console.log(num);
+  }
 
 }
